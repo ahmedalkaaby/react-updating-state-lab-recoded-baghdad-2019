03f206a6fa814cc5320ceaa84a40e16e8996d096
K3be <ahmed.13.alkaaby@gmail.com>
2020-01-05 20:24:36 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component{
+  constructor(){
+    this.state({
+      timeClicked: 0
+    })
+  }
+  handleClick=()=>{
+    this.setState(previousState=>{
+      return{
+      timeClicked=previousState.timeClicked+1
+    }
+    })
+  }
+
+  render(){
+    return(
+      <button onClick={this.handleClick}>{this.state.timeClicked}</button>
+    )
+  }
+}
